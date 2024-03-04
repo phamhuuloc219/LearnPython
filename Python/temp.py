17e38a8d2e41ef1d17e766484e2d90e47daec2ca
phamhuuloc219 <94950295+phamhuuloc219@users.noreply.github.com>
2024-03-04 14:45:03 +0700

    review silde1-4

--- Python/temp.py
+++ Python/temp.py
@@ -1,12 +1,23 @@
+# #Slide 2
+# birth_year=int(input("Enter your birth year: "))
+# current_year=2024
+# age=current_year-birth_year
+# print("Current age: ", age)
 
+# #Slide 3
+# length_rec=int(input("Enter your length rectangle: "))
+# width_rec=int(input("Enter your width rectangle: "))
+# for i in range (length_rec) :
+#     for j in range (width_rec) :
+#         print("*",end="")
+#     print()
 
-
-
-
-
-
-
-
-
-
-
+# #Slide 4
+# count=0
+# sentence="Hello, I'm PHL, a student at NTU. Today, I learning Python. I'm fine"
+# check=str(input("Enter a word: "))
+# sentence_chuyen=sentence.split()
+# for i in sentence_chuyen :
+#     if(i==check):
+#         count+=1
+# print(count)
