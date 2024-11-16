6d28520a8d98ca4578d55cad6eb4ed35992d36f2
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-11-16 22:44:44 +0700

    Phu Learn Python 5

---
+++ Python/python_phu/buoi-5.py
@@ -0,0 +1,28 @@
+# cấu trúc if-else
+
+# if (điều kiện) :
+#     # code thực thi nếu điều kiện đúng
+
+n = int(input("n= "))
+
+# if ( n %2 ==0):
+#     print("n là số chẵn")
+# else: # n % 2 != 0
+#     print("n là số lẻ")
+
+if(n ==0 ):
+    print("n là số 0")
+    
+if(n>0):
+    print("n là số dương")
+else:
+    print("n là số âm")
+
+# Bài 1
+# Viết chương trình kiểm tra mật khẩu và in ra "Mật khẩu đúng" nếu mật khẩu là "python", ngược lại in ra "Mật khẩu sai".
+# Bài 2
+# Viết chương trình yêu cầu người dùng nhập vào số đại diện cho ngày trong tuần (1-7).
+# Kiểm tra và in ra thông điệp tương ứng với ngày đó (1: Chủ nhật, 2: Thứ hai,...,7: Thứ bảy)
+# Bài 3
+# Viết chương trình yêu cầu người dùng nhập vào giờ (24 giờ).
+# Kiểm tra và in ra thông điệp dựa trên giờ: buổi sáng (5-11 giờ), buổi chiều (12-17 giờ), buổi tối (18-22 giờ), ban đêm (23-4 giờ).
