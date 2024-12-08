f87b1a9853a6f70e49b6634ab3bc3832525a66e8
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-12-08 22:06:46 +0700

    password python

--- Python/python_phu/buoi-10.py
+++ Python/python_phu/buoi-10.py
@@ -4,25 +4,38 @@
 #     dem += 1
 # print("goodbye")
 
-#Bài 1: Dùng vòng lặp while in ra các số từ 1 đến 10
-n = 1
-while (n<=10):
-    print(n)
-    n += 1
+# #Bài 1: Dùng vòng lặp while in ra các số từ 1 đến 10
+# n = 1
+# while (n<=10):
+#     print(n)
+#     n += 1
 
-#Bài 2: Dùng vòng lặp while in ra các số từ 10 đến 1
-m = 10
-while (m >= 1):
-    print(m)
-    m -= 1
+# #Bài 2: Dùng vòng lặp while in ra các số từ 10 đến 1
+# m = 10
+# while (m >= 1):
+#     print(m)
+#     m -= 1
     
-#Bài 3: Dùng vòng lặp while in ra các số chẵn từ 1 đến 20
-i = 1
-while (i <= 20):
-    if(i % 2 == 0):
-        print(i)
-    i += 1
-#Bài 4: Dùng vòng lặp while in ra các số lẻ từ 1 đến 20
+# #Bài 3: Dùng vòng lặp while in ra các số chẵn từ 1 đến 20
+# i = 1
+# while (i <= 20):
+#     if(i % 2 == 0):
+#         print(i)
+#     i += 1
+# #Bài 4: Dùng vòng lặp while in ra các số lẻ từ 1 đến 20
+# n = 1
+# while(n<=20):
+#     if (n % 2 != 0):
+#         print(n)
+#     n+=1
 #Bài 5: Dùng vòng lặp while để kiểm tra mật khẩu nhập vào có phải là "Python" hay không.
 # Nếu là "Python" thì in ra "Đúng mật khẩu" và thoát vòng lặp, nếu không thì in ra "Sai mật khẩu"
 # và yêu cầu người dùng nhập lại mật khẩu.
+passwords = "python"
+while True:
+    passwords = input("Enter your password: ")
+    if passwords == "python":
+        print("Đúng mật khẩu")
+        break
+    else:
+        print("Sai mật khẩu. Vui lòng nhập lại.")
