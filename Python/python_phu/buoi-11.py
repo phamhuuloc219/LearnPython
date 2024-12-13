a92e0fabaac9a61bf54edf6a973f8e0ad1e5033d
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-12-14 00:01:37 +0700

    add exercise 11

---
+++ Python/python_phu/buoi-11.py
@@ -0,0 +1,5 @@
+# 1. Viết chương trình sử dụng vòng lặp while nhập 1 số nguyên dương n.
+# 2. Viết chương trình sử dụng vòng lặp while tính tổng của các số từ 1 đến n.
+# 3. Viết chương trình sử dụng vòng lặp while tính tổng các chữ số của một số nguyên dương.
+# 4. Viết chương trình nhập vào 1 số nguyên dương và kiểm tra số chẵn hay lẻ.
+# 5. Viết chương trình nhập vào điểm Toán, Tiếng Anh và Tiếng Việt và tính điểm trung bình.  
