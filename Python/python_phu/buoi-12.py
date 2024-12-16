45c9cf8e064f1bdfad8e70a808f442ce82dab14c
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-12-16 19:12:09 +0700

    add exercise 122

---
+++ Python/python_phu/buoi-12.py
@@ -0,0 +1,5 @@
+# 1.  Nhập vào hai số nguyên dương a và b. Tìm tổng của tất cả các số lẻ và chẵn nằm giữa hai số đó.
+# 2. Viết chương trình yêu cầu người dùng đoán một số bí mật trong khoảng từ 1 đến 100. Chương trình
+# sẽ tiếp tục yêu cầu người dùng đoán cho đến khi họ đoán đúng. Mỗi lần đoán, chương trình sẽ cho biết
+# số người dùng đoán là lớn hơn hay nhỏ hơn số bí mật.
+# 3. Nhập 2 số nguyên x và y. Viết chương trình tính tổng bình phương các số từ x đến y.
