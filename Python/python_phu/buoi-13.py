b04ed2450699e91e1e2be6dbdd4c38f7822b5d08
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-12-24 22:57:14 +0700

    add exercise 13

---
+++ Python/python_phu/buoi-13.py
@@ -0,0 +1,5 @@
+# 1. Viết chương trình nhập vào số nguyên dương n. Tìm các số chia hết cho 3 và 5 trong khoảng từ 1 đến n
+# 2. Viết chương trình tính giá trị của biểu thức S= 1.2 + 2.3 + 3.4 + ... + n(n+1).
+# 3. Viết chương trình tính tiền lương khi làm qua số giờ tiêu chuẩn theo quy định của công ty (tăng ca) của 1 nhân viên.
+# Biết số giờ tiêu chuẩn là 44 giờ, lương theo giờ là 25$/h. Nếu làm tăng ca thì tính 30$/h. Hãy tính số lương thực lãnh của nhân viên.
+# Gợi ý: Đầu vào là 1 số lớn hơn hoặc bằng 44 
