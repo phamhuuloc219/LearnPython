aa7ba04a547b2f6b5761f468d368a7f041368818
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2025-01-15 23:18:23 +0700

    test

---
+++ Python/python_phu/test.py
@@ -0,0 +1,9 @@
+a = input(int())
+while ( a< 0 or a > 100):
+    a = input(int())
+b = input(int())
+while ( b< 0 or b > 100):
+    b = input(int())
+while ( (a+b)<100 or (a+b)>200):
+    a = input(int())
+    b = input(int())
