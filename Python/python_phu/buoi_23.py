8535b576e0b98ffeed8b457c5b5cf83f8f39e53a
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2025-03-15 11:37:58 +0700

    Phu learn Python 24

--- Python/python_phu/buoi_23.py
+++ Python/python_phu/buoi_23.py
@@ -1,60 +1,63 @@
-# Viết chương trình nhập một chuỗi và in ra chuỗi đảo ngược.
-# python => nohtyp
+# # Viết chương trình nhập một chuỗi và in ra chuỗi đảo ngược.
+# # python => nohtyp
 
-chuoi = input("Nhập chuỗi: ")
-chuoi_dao_nguoc = ""
+# chuoi = input("Nhập chuỗi: ")
+# chuoi_dao_nguoc = ""
 
-for i in chuoi:
-    chuoi_dao_nguoc = i + chuoi_dao_nguoc
+# for i in chuoi:
+#     chuoi_dao_nguoc = i + chuoi_dao_nguoc
     
-# print("Chuỗi sau khi đảo ngược:", chuoi_dao_nguoc)
-if chuoi == chuoi_dao_nguoc:
-    ...
-else:
-    ...
+# # print("Chuỗi sau khi đảo ngược:", chuoi_dao_nguoc)
+# if chuoi == chuoi_dao_nguoc:
+#     ...
+# else:
+#     ...
     
     
-# Phần tử
-my_list = ["apple", "orange", 4.5, 6, 8.5, True, False, [3,6,2,5,5]]
-# Truy xuất phần tử đầu tiên
-print(my_list[0])
-# Truy xuất phần tử cuối cùng
-print(my_list[-1])
-# Kiểm tra phần tử có trong danh sách
-print("orange" in my_list)
-# Nối 2 danh sách
-list1 = [4,6,2,6,7,3]
-list2 = ['orange1', 'orange2',True]
-new_list = list1 + list2
-print(new_list)
+# # Phần tử
+# my_list = ["apple", "orange", 4.5, 6, 8.5, True, False, [3,6,2,5,5]]
+# # Truy xuất phần tử đầu tiên
+# print(my_list[0])
+# # Truy xuất phần tử cuối cùng
+# print(my_list[-1])
+# # Kiểm tra phần tử có trong danh sách
+# print("orange" in my_list)
+# # Nối 2 danh sách
+# list1 = [4,6,2,6,7,3]
+# list2 = ['orange1', 'orange2',True]
+# new_list = list1 + list2
+# print(new_list)
 
 
 
 
-list1 = [3,1,2,3,4,3,5]
+list1 = [1, 5, 3, 5, 5, 6, 7, 7]
+# list2 = ['orange1', 'orange2']
 # Thêm 1 phần tử vào cuối danh sách append()
-list1.append(1111)
+list1.append(999)
 print(list1)
-
-# Thêm phần tử vào vị trí bất kỳ trong danh sách
-# insert(vị trí, giá trị)
-# Thêm phần tử apple vào vị trí thứ 4
-list1.insert(3,"apple")
-print(list1)
-
+# list2.append("banana")
+# print(list2)
+# # Thêm phần tử vào vị trí bất kỳ trong danh sách
+# # insert(vị trí, giá trị)
+# # Thêm phần tử apple vào vị trí thứ 4
+# list1.insert(3,"apple")
+# print(list1)
+# list2.insert(2,"Iphone")
+# print(list2)
 
 # Xóa 1 phần tử đầu tiên có giá trị trùng
 # với giá trị được truyển vào trong danh sách
-list1.remove(3)
-print(list1)
+# list1.remove(5)
+# print(list1)
 
-# Xóa phần tử theo chỉ số(index): pop()
-# pop(index)
-# Mặc định nếu không truyền index vào
-# thì giá trị bị xóa là gía trị cuối cùng
-list1.pop()
-# xóa phần tử thứ 4
-list1.pop(3)
+# # Xóa phần tử theo chỉ số(index): pop()
+# # pop(index)
+# # Mặc định nếu không truyền index vào
+# # thì giá trị bị xóa là gía trị cuối cùng
+# list1.pop()
+# # xóa phần tử thứ 4
+list1.pop(6)
 print(list1)
 
 
---
+++ Python/python_phu/buoi_24.py
@@ -0,0 +1,64 @@
+# Nhập một danh sách từ bàn phím
+# # Cách 1:
+# arr = input("Nhập các phần tử của danh sách (Cách nhau bởi 1 khoảng trắng): ") 
+# # tách chuỗi vừa nhập thành danh sách
+# arr = arr.split(";")
+# print(arr)
+
+# Cách 2:
+# danh_sach = []
+# n = int(input("Nhập số lượng phần tử có trong danh sách: "))
+# for i in range(n):
+#     arr = int(input(f"Nhập phần tử thứ {i+1}: "))
+#     danh_sach.append(arr)
+# print(danh_sach)
+    
+# 1. Viết chương trình nhập vào 1 danh sách các số nguyên gồm 10 phần tử.
+# In ra danh sách các số chẵn có trong danh sách
+
+danh_sach = []
+n = int(input("Nhập số lượng phần tử có trong danh sách: "))
+for i in range(n):
+    arr = int(input(f"Nhập phần tử thứ {i+1}: "))
+    danh_sach.append(arr)
+print(danh_sach)
+# print("Các số chẵn có trong danh sách là: ")
+# for i in range(len(danh_sach)):
+#     if danh_sach[i] % 2 == 0:
+#         print(danh_sach[i], end=" ")
+
+# 2. Viết chương trình nhập vào 1 danh sách các số nguyên gồm 10 phần tử.
+# In ra danh sách các số lẻ và chia hết cho 3 có trong danh sách
+
+# 3. Viết chương trình nhập vào 1 danh sách các số nguyên gồm 10 phần tử.
+# Tính tổng các phần tử có trong danh sách
+
+# tong = 0
+# for i in range(len(danh_sach)):
+#     tong += danh_sach[i]
+#     tong = tong + danh_sach[i]
+    
+# 4. Viết chương trình cho danh sách 10 phần tử True/False bất kỳ.
+# Tính số lượng giá trị True có trong danh sách.
+# temp = ["True", "False", "True", "False", "True", "False", "True", "False", "True", "False"]
+# dem = 0
+# for i in range(len(temp)):
+#     if temp[i] == "True":
+#         dem += 1
+# print("số lượng giá trị True có trong danh sách: ",dem)
+
+# 5. Viết chương trình nhập vào 1 danh sách các số nguyên gồm n phần tử.
+# sắp xếp danh sách theo thứ tự tăng dần và giảm dần
+
+ds = [5,7,1,534,76,23,956,342,745]
+# Sắp xếp tăng dần
+ds.sort()
+print("Sắp xếp theo thứ tự tăng dần: ",ds)
+
+# Sắp xếp giảm dần
+ds.sort(reverse=True)
+print("Sắp xếp theo thứ tự giảm dần: ",ds)
+
+# 6. Viết chương trình nhập vào 1 danh sách các số nguyên gồm n phần tử.
+# Đếm số lượng phần tử xuất hiện nhiều nhất trong danh sách.
+# Ví dụ: ["apple", "orange, "apple", "orange", "apple", "blue", "red","blue"]
