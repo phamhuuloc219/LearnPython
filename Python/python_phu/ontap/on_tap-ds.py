e9ebb8b03b2974667453f318870264451d480b9c
locpham <loc.pham@sweetsoft.vn>
2025-12-07 11:20:45 +0700

    .

--- Python/python_phu/ontap/on_tap-ds.py
+++ Python/python_phu/ontap/on_tap-ds.py
@@ -1,13 +1,15 @@
 n = int(input("Nhập số phần tử: "))
 
 danh_sach = []
-tong = 0
 
 for i in range(n):
     so = int(input(f"Nhập giá trị phần tử thứ {i + 1}: "))
     danh_sach.append(so)
 
-for i in range(len(danh_sach)):
-    if danh_sach[i] % 7 == 0:
-	    tong += danh_sach[i]
-print("Tổng các số chia hết cho 7:", tong)
+max = danh_sach[0] # Giả sử phần tử đầu tiên là lớn nhất
+
+for phan_tu in danh_sach:
+    if phan_tu > max:
+        max = phan_tu
+        
+print("Giá trị lớn nhất trong danh sách là:", max)
